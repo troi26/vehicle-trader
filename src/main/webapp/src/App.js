f60c46c57c5038e958480602d0378914b4988a45
TrayanDTroev <trayan97@gmail.com>
2020-02-06 09:08:40 +0200

    Sample of how to wire custom components to the App container component, in this case Users`, Offers` and Bids` previeweres.

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -7,16 +7,42 @@ import { UsersPreviewerContainer } from "./components/UsersPreviewer/UsersPrevie
 import {LoginPageContainer} from "./components/LoginPage/LoginPageContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {HeaderContainer} from "./components/Header/HeaderContainer";
+import {OffersPreviewerContainer} from "./components/OffersPreviewer/OffersPreviewerContainer";
+import {BidsPreviewerContainer} from "./components/BidsPreviewer/BidsPreviewerContainer";
 
 function App() {
   return (
-    <div className="App" style={{
-        width: `${window.innerWidth}px`,
-        height: `${window.innerHeight}px`,
+    <div className="App inv-scroll" style={{
+        position: 'relative',
+        padding: '0px',
+        width: `100%`,
+        height: `100%`,
     }}>
         <HeaderContainer />
-        <LoginPageContainer />
-        <UsersPreviewerContainer />
+        {/*<LoginPageContainer />*/}
+        <BidsPreviewerContainer
+            style={{
+                margin: '0.5em',
+                minHeight: `${window.innerHeight / 3}px`,
+                height: '33%',
+                overflowY: 'auto',
+            }}/>
+        <OffersPreviewerContainer
+            style={{
+                margin: '0.5em',
+                height: '33%',
+                minHeight: `${window.innerHeight / 3}px`,
+                overflowY: 'auto',
+            }}
+        />
+        <UsersPreviewerContainer
+            style={{
+                margin: '0.5em',
+                height: '33%',
+                minHeight: `${window.innerHeight / 3}px`,
+                overflowY: 'auto',
+            }}
+        />
     </div>
   );
 }
