44f424de9bf15e1358dc1db19ad4eb8eef940a00
TrayanDTroev <trayan97@gmail.com>
2020-02-06 09:01:13 +0200

    (WEBAPP): Updated endpoint calls and added bids get stream subscription call.

--- src/main/webapp/src/api/BidsFetchAPI.js
+++ src/main/webapp/src/api/BidsFetchAPI.js
@@ -1,1 +1,10 @@
 import { BIDS } from "./Endpoints";
+
+/**
+ * Method which subscribes to the endpoint which streams all the bids to given offer
+ * @param offerId - the ID of the offer
+ * @returns {EventSource} - returns the subscription object
+ */
+export const getBidsByOfferId = (offerId) => {
+    return new EventSource(`${BIDS.GET_BIDS_OF_OFFER}?offerId=${offerId}`);
+};
