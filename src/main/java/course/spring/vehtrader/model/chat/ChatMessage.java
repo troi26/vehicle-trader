8830eff4fd2267005c4f395bd5ce1fe8d28fb98d
i356280 <v.katrankiev@sap.com>
2020-02-07 15:08:57 +0200

    implementedd chat functionality (this time working properly)

--- src/main/java/course/spring/vehtrader/model/chat/ChatMessage.java
+++ src/main/java/course/spring/vehtrader/model/chat/ChatMessage.java
@@ -1,5 +1,6 @@
 package course.spring.vehtrader.model.chat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import course.spring.vehtrader.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,23 +10,37 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Document("chat_conversations")
+@Document("chat_messages")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatConversation {
+public class ChatMessage {
     @Id
     String id;
 
     @NonNull
     @NotNull
-    User user1;
+    String channelId;
 
     @NonNull
     @NotNull
-    User user2;
+    String sender;
 
-    List<Message> messageHistory;
+    @NonNull
+    @NotNull
+    String receiver;
+
+    @NonNull
+    @NotNull
+    @Size(min = 1)
+    String message;
+
+    @NonNull
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime messageDateTime;
 }
---
+++ src/main/java/course/spring/vehtrader/repo/chat/ChatMessageRepository.java
@@ -0,0 +1,14 @@
+package course.spring.vehtrader.repo.chat;
+
+import course.spring.vehtrader.model.chat.ChatMessage;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, String> {
+    @Tailable
+    Flux<ChatMessage> findByChannelId(String channelId);
+
+    @Tailable
+    Flux<ChatMessage> findBySenderAndReceiver(String sender, String receiver);
+}
