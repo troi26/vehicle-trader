7c0eafcad07ed4f9dca139ff28849b97ca7bae58
TrayanDTroev <trayan97@gmail.com>
2020-02-10 19:35:11 +0200

    Added controller methods for user register, get active and not active accounts and get users without specific user by id.

--- src/main/java/course/spring/vehtrader/web/UsersController.java
+++ src/main/java/course/spring/vehtrader/web/UsersController.java
@@ -2,20 +2,24 @@ package course.spring.vehtrader.web;
 
 import course.spring.vehtrader.domain.UsersService;
 import course.spring.vehtrader.exceptions.InvalidEntityException;
+import course.spring.vehtrader.exceptions.NonExistingEntityException;
 import course.spring.vehtrader.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @CrossOrigin
@@ -28,6 +32,43 @@ public class UsersController {
     @GetMapping
     public List<User> getAllUsers() {
         return usersService.findAll();
+    }
+
+    @GetMapping(path = "/accounts/inactive")
+    public List<User> getAllInactiveAccounts() {
+        return usersService.findInactiveAccounts();
+    }
+
+    @GetMapping(path = "/accounts/active")
+    public List<User> getAllActiveAccounts() {
+        return usersService.findActiveAccounts();
+    }
+
+    @GetMapping(path = "/accounts/active/others", params = "id")
+    public List<User> getAllActiveAccountsNotMe(@RequestParam("id") String myId) {
+        return usersService.findActiveAccountsNotMe(myId);
+    }
+
+    @PutMapping(path = "/accounts/activate", params = "id")
+    public String activateAccount(@RequestParam("id") String userId) {
+        User account = usersService.findById(userId);
+        account.setActive(true);
+        String id = usersService.update(account).getId();
+        return id;
+    }
+
+    @PutMapping(path = "/accounts/deactivate", params = "id")
+    public String deactivateAccount(@RequestParam("id") String userId) {
+        User account = usersService.findById(userId);
+        account.setActive(false);
+        String id = usersService.update(account).getId();
+        return id;
+    }
+
+    @GetMapping(path = "/accounts/excluded", params = "id")
+    public List<User> getAllUsersExcludedId(@RequestParam("id") String exclUserId) {
+        List<User> accounts = usersService.findAllByIdNot(exclUserId);
+        return accounts;
     }
 
     @GetMapping(params = "username")
@@ -44,6 +85,37 @@ public class UsersController {
     @PostMapping
     public User insertUser(@RequestBody User user) {
       return usersService.create(user);
+    }
+
+//    @RequestMapping(path = "/register", method = RequestMethod.POST,
+//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(path = "/register")
+    public ResponseEntity<String> createAccountUser(@Valid @RequestBody User user/*MultiValueMap<String, String> userForm*/,
+                                                    HttpServletResponse response) {
+//        User user = new User(userForm.getFirst("username"),
+//                userForm.getFirst("name"), userForm.getFirst("surname"),
+//                userForm.getFirst("email"), Double.parseDouble(userForm.getFirst("cashAmount")),
+//                userForm.getFirst("password"), userForm.getFirst("roles"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        response.setContentType("application/json");
+        try {
+            usersService.findByUsername(user.getUsername());
+        } catch (NonExistingEntityException ex) {
+            user.setActive(false);
+            usersService.create(user);
+            return new ResponseEntity<>(String.format("{ \"registered\": %b }", true),
+                    headers, HttpStatus.CREATED);
+        }
+
+
+        return new ResponseEntity<>(String.format("{ \"registered\": %b, \"errorMsg\": \"%s\", \"field\": \"%s\" }",
+                false, "Username already taken", "username"),
+                headers, HttpStatus.CONFLICT);
+//        ResponseEntity<String> response1 = new ResponseEntity<>(,
+//                HttpStatus.CONFLICT);
+//        return response;
     }
 
     @CrossOrigin
