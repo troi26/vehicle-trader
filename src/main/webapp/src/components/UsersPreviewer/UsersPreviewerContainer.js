9ba704c295dc970747ef56dd96d5e450d0ffa2ca
TrayanDTroev <trayan97@gmail.com>
2020-02-10 19:45:24 +0200

    (WEBAPP): Added activate/deactivate buttons and functionality to users list previewer.

--- src/main/webapp/src/components/UsersPreviewer/UsersPreviewerContainer.js
+++ src/main/webapp/src/components/UsersPreviewer/UsersPreviewerContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 import { UsersPreviewerView } from './UsersPreviewerView';
-import { getAllUsers } from '../../api/UsersFetchAPI';
+import {
+    activateAcc,
+    deactivateAcc,
+    getAllActiveAccountsNotMe,
+    getAllIgnoringId,
+    getAllUsers
+} from '../../api/UsersFetchAPI';
+import {PendingUsersListView} from "../PendingUsersList/PendingUsersListView";
 
 export class UsersPreviewerContainer extends Component {
     constructor (props) {
@@ -15,20 +22,39 @@ export class UsersPreviewerContainer extends Component {
     }
 
     componentDidMount () {
-        this.interval = setTimeout(() => {
-            getAllUsers(this.props.authToken)
+        if (this.props.loggedIn.roles === "ROLE_ADMIN") {
+            getAllIgnoringId(this.props.loggedIn.id)
                 .then((response) => {
-                    return response.json();
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                    throw "Unsuccessful users loading...";
                 }).then((response) => {
-                    console.log(response);
-                    this.setState({
-                        users: response,
-                    });
-                })
+                console.log(response);
+                this.setState({
+                    users: response,
+                });
+            })
                 .catch((reason) => {
                     console.log(reason);
                 });
-        }, 1000);
+        } else {
+            getAllActiveAccountsNotMe(this.props.loggedIn.id)
+                .then((response) => {
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                    throw "Unsuccessful users loading...";
+                }).then((response) => {
+                console.log(response);
+                this.setState({
+                    users: response,
+                });
+            })
+                .catch((reason) => {
+                    console.log(reason);
+                });
+        }
     }
 
     componentWillUnmount() {
@@ -36,13 +62,65 @@ export class UsersPreviewerContainer extends Component {
         this.interval = null;
     }
 
+    activateAccountHandler (userId) {
+        activateAcc(userId)
+            .then(r => {
+                if (r.status === 200) {
+                    console.log(r);
+                    return r.text();
+                }
+                throw "Unsuccessful Activation";
+            })
+            .then(activatedId => {
+                console.log(activatedId);
+                this.setState({
+                    users: this.state.users
+                        .map(acc => acc.id === activatedId
+                            ? {
+                                ...acc,
+                                active: true,
+                            }
+                            : acc),
+                })
+            })
+            .catch(reason => console.log(reason));
+    }
+
+    deactivateAccountHandler (userId) {
+        deactivateAcc(userId)
+            .then(r => {
+                if (r.status === 200) {
+                    console.log(r);
+                    return r.text();
+                }
+                throw "Unsuccessful Deactivation";
+            })
+            .then(deactivatedId => {
+                console.log(deactivatedId);
+                this.setState({
+                    users: this.state.users
+                        .map(acc => acc.id === deactivatedId
+                            ? {
+                                ...acc,
+                                active: false,
+                            }
+                            : acc),
+                })
+            })
+            .catch(reason => console.log(reason));
+    }
+
     render() {
         return (
             <UsersPreviewerView
                 {...this.state}
-               style={{
-                   ...this.props.style,
-               }}
+                {...this.props}
+               // style={{
+               //     ...this.props.style,
+               // }}
+
+                onActivateAccount={this.activateAccountHandler.bind(this)}
+                onDeactivateAccount={this.deactivateAccountHandler.bind(this)}
             />
         );
 
