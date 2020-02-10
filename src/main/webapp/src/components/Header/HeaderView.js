99e2db16075be951c4441ce95137e7db36bb0510
TrayanDTroev <trayan97@gmail.com>
2020-02-10 10:57:35 +0200

    (WEBAPP): Added loggedin info to the nav bar and some options for navigation through the menus.

--- src/main/webapp/src/components/Header/HeaderView.js
+++ src/main/webapp/src/components/Header/HeaderView.js
@@ -1,42 +1,76 @@
 import React, {useState} from "react";
-import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
-import Col from "reactstrap/es/Col";
-import Dropdown from "reactstrap/es/Dropdown";
+import {
+    Collapse, DropdownItem, DropdownMenu,
+    DropdownToggle,
+    Nav,
+    Navbar,
+    NavbarBrand, NavbarText,
+    NavbarToggler,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown
+} from "reactstrap";
 
 export const HeaderView = (props) => {
 
-    const [collapsed, setCollapsed] = useState(true);
-    const toggleNavbar = () => setCollapsed(!collapsed);
-
     return (
-        <Navbar color="faded" light>
-            <NavbarBrand href="/" className="mr-auto">HODINI Trader</NavbarBrand>
-            <Col>
-                {/*<Dropdown>*/}
-                    <img src={props.loggedIn.avatarUrl
-                        ? `http://localhost:8080/uploads/${props.loggedIn.avatarUrl}`
-                        : "http://localhost:8080/uploads/no_avatar.png"}
-                        className={'vt-avatar-img'}
-                    />
-                {/*</Dropdown>*/}
-
-            </Col>
-            <Col>{props.loggedIn.username}</Col>
-            <Col>{`${props.loggedIn.name} ${props.loggedIn.surname}`}</Col>
-            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!collapsed} navbar>
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink onClick={props.onLoginClick} href={"#"}>Login</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink onClick={props.onHomeClick} href={"#"}>Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink onClick={props.onLogoutClick} href={"#"}>Logout</NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    )
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">HOUDINI Trader</NavbarBrand>
+                <NavbarToggler onClick={() => console.log()} />
+                <Collapse isOpen={true} navbar>
+                    <Nav className="mr-auto" navbar>
+                        {/*<NavItem>*/}
+                        {/*    <NavLink href="#">Home</NavLink>*/}
+                        {/*</NavItem>*/}
+                        <NavItem>
+                            <NavLink href="#"
+                                     onClick={props.onOffersListShowClick}>Browse offers</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <Nav className="mr-right" navbar>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                <img src={props.loggedIn.avatarUrl
+                                    ? `http://localhost:8080/uploads/${props.loggedIn.avatarUrl}`
+                                    : "http://localhost:8080/uploads/no_avatar.png"}
+                                     className={'vt-avatar-img'}/*
+                                     onClick={props.onShowAccountInfoClick}*/
+                                />
+                                {` ${props.loggedIn.username}`}
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem
+                                    onClick={props.onShowAccountInfoClick}>
+                                    {`${props.loggedIn.name} ${props.loggedIn.surname}`}
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={(event) => props.onOffersShowClick(props.loggedIn.id, event)}>
+                                    My offers
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={(event) => props.onBidsShowClick(props.loggedIn.id, event)}>
+                                    My bids
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={(event) => props.onOpenMessengerClick(props.loggedIn.id, event)}>
+                                    Chat
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={(event) => props.onOpenForumClick(props.loggedIn.id, event)}>
+                                    My posts
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem
+                                    href={"http://localhost:8080/perform_logout"}
+                                >
+                                    Logout
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
 }
