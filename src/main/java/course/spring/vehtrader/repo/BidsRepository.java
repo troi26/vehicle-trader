a63c2ab71690ecff0007be3fb12f9f6285489cad
TrayanDTroev <trayan97@gmail.com>
2020-02-10 19:29:36 +0200

    Added bids cnt and bids by offer id. Also get users active and inactive accounts.

--- src/main/java/course/spring/vehtrader/repo/BidsRepository.java
+++ src/main/java/course/spring/vehtrader/repo/BidsRepository.java
@@ -4,6 +4,10 @@ import course.spring.vehtrader.model.Bid;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BidsRepository extends ReactiveMongoRepository<Bid, String> {
     @Tailable
@@ -11,4 +15,10 @@ public interface BidsRepository extends ReactiveMongoRepository<Bid, String> {
 
     @Tailable
     public Flux<Bid> findAllByUserId (String userId);
+
+
+    public Flux<Bid> findByOfferIdOrderByValue (String offerId);
+
+
+    public Mono<Long> countAllByOfferId(String offerId);
 }
--- src/main/java/course/spring/vehtrader/repo/UsersRepository.java
+++ src/main/java/course/spring/vehtrader/repo/UsersRepository.java
@@ -3,8 +3,14 @@ package course.spring.vehtrader.repo;
 import course.spring.vehtrader.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsersRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
+    List<User> findAllByActiveFalse();
+    List<User> findAllByActiveTrue();
+    List<User> findAllByActiveTrueAndIdNot(String id);
+
+    List<User> findAllByIdNot(String excludedId);
 }
