0eb9d64050b4d9976c927109304a5aa08dd4972f
TrayanDTroev <trayan97@gmail.com>
2020-02-11 13:29:21 +0200

    Data initializer source commented for now.

--- src/main/java/course/spring/vehtrader/init/DataInitializer.java
+++ src/main/java/course/spring/vehtrader/init/DataInitializer.java
@@ -1,30 +1,30 @@
-package course.spring.vehtrader.init;
-
-import course.spring.vehtrader.domain.UserService;
-import course.spring.vehtrader.model.User;
-import lombok.extern.apachecommons.CommonsLog;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@CommonsLog
-@Component
-public class DataInitializer implements ApplicationRunner {
-    @Autowired
-    private UserService usersService;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        long cnt = usersService.getCount();
-        System.out.printf("USERS: %d", cnt);
-        if (cnt == 0) {
-            System.out.printf("USERS: %d", cnt);
-            User user = new User("admin", "Admin", "Admin", "admin@gmail.com",
-                    "aDmin123&");
-            usersService.create(user);
-        }
-    }
-}
+//package course.spring.vehtrader.init;
+//
+//import course.spring.vehtrader.domain.UserService;
+//import course.spring.vehtrader.model.User;
+//import lombok.extern.apachecommons.CommonsLog;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@CommonsLog
+//@Component
+//public class DataInitializer implements ApplicationRunner {
+//    @Autowired
+//    private UserService usersService;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        long cnt = usersService.getCount();
+//        System.out.printf("USERS: %d", cnt);
+//        if (cnt == 0) {
+//            System.out.printf("USERS: %d", cnt);
+//            User user = new User("admin", "Admin", "Admin", "admin@gmail.com",
+//                    "aDmin123&");
+//            usersService.create(user);
+//        }
+//    }
+//}
